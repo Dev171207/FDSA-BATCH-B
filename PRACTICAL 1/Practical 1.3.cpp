34b2dcf9ea39ac9154dfa66cddab9a40120771a0
Dev Pranav Pandya <devpandya6777@gmail.com>
2026-08-03 14:07:15 +0530

    Add files via upload

---
+++ PRACTICAL 1/Practical 1.3.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<string>
+#include<sstream>
+using namespace std;
+
+int main()
+{
+    string sen;
+    string word, longest_word;
+    int max_length =0;
+    cout <<"Describe a article over here  :";
+    getline(cin,sen);
+
+    stringstream ss(sen);
+    while(ss >> word )
+    {
+        if( word.length() > max_length)
+        {
+            max_length = word.length();
+            longest_word =word;
+        }
+    }
+
+    cout<<"Longest word :"<<longest_word <<endl;
+    cout<<"Max Length :"<<max_length <<endl;
+    return 0;
+}
